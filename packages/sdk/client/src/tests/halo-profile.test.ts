6085c604c5d15e767d067c29aaedbd4424b02262
Nathan A. Ferch <nf+github@marginal.net>
2024-02-02 17:38:23 -0500

    add unit tests

--- packages/sdk/client/src/tests/halo-profile.test.ts
+++ packages/sdk/client/src/tests/halo-profile.test.ts
@@ -4,7 +4,10 @@
 
 import { expect } from 'chai';
 
+import { Trigger, asyncTimeout, sleep } from '@dxos/async';
 import { performInvitation } from '@dxos/client-services/testing';
+import { invariant } from '@dxos/invariant';
+import { log } from '@dxos/log';
 import { describe, test, afterTest } from '@dxos/test';
 
 import { Client } from '../client';
@@ -59,5 +62,80 @@ describe('Halo', () => {
 
     expect(await client1.halo.devices.get()).to.have.lengthOf(2);
     expect(await client2.halo.devices.get()).to.have.lengthOf(2);
+  });
+
+  test.skip('profile update is visible to other devices', async () => {
+    const testBuilder = new TestBuilder();
+
+    const client1 = new Client({ services: testBuilder.createLocal() });
+    afterTest(() => client1.destroy());
+    await client1.initialize();
+
+    await client1.halo.createIdentity({ displayName: 'test-user' });
+    expect(client1.halo.identity).exist;
+
+    expect(await client1.halo.devices.get()).to.have.lengthOf(1);
+
+    const client2 = new Client({ services: testBuilder.createLocal() });
+    afterTest(() => client2.destroy());
+    await client2.initialize();
+
+    await Promise.all(performInvitation({ host: client1.halo, guest: client2.halo }));
+
+    expect(await client1.halo.devices.get()).to.have.lengthOf(2);
+
+    const trigger = new Trigger();
+    client2.halo.identity.subscribe((identity) => {
+      if (identity?.profile?.displayName === 'test-user-updated') {
+        trigger.wake();
+      }
+    });
+
+    await client1.halo.updateProfile({ displayName: 'test-user-updated' });
+    await asyncTimeout(trigger.wait(), 500);
+
+    expect(client2.halo.identity.get()!.profile?.displayName).to.equal('test-user-updated');
+  });
+
+  test.only('device profile update is visible to other devices', async () => {
+    const testBuilder = new TestBuilder();
+
+    const client1 = new Client({ services: testBuilder.createLocal() });
+    afterTest(() => client1.destroy());
+    await client1.initialize();
+
+    await client1.halo.createIdentity({ displayName: 'test-user' });
+    expect(client1.halo.identity).exist;
+
+    expect(await client1.halo.devices.get()).to.have.lengthOf(1);
+
+    const client2 = new Client({ services: testBuilder.createLocal() });
+    afterTest(() => client2.destroy());
+    await client2.initialize();
+
+    await Promise.all(performInvitation({ host: client1.halo, guest: client2.halo }));
+
+    expect(await client1.halo.devices.get()).to.have.lengthOf(2);
+    expect(await client2.halo.devices.get()).to.have.lengthOf(2);
+
+    invariant(client1.services.services?.DevicesService, 'DevicesService is not available');
+    await client1?.services?.services?.DevicesService?.updateDevice({
+      label: 'updated-device-profile',
+    });
+
+    const trigger = new Trigger();
+    client2.halo.devices.subscribe((devices) => {
+      if (
+        devices.find((device) => device.deviceKey !== client1.halo.device?.deviceKey)?.profile?.label ===
+        'updated-device-profile'
+      ) {
+        trigger.wake();
+      }
+    });
+
+    await asyncTimeout(trigger.wait(), 500);
+    expect(
+      client2.halo.devices.get().find((device) => device.deviceKey !== client1.halo.device?.deviceKey)?.profile?.label,
+    ).to.equal('updated-device-profile');
   });
 });
