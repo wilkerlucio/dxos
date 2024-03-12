41b1e8c4b94569303fc00fd02d37641e99794525
Josiah Witt <josiah@witt.life>
2024-03-11 20:30:11 -0400

    wip: query result

--- packages/sdk/rql/src/rql.ts
+++ packages/sdk/rql/src/rql.ts
@@ -11,7 +11,7 @@ interface Resolvers {
   readonly resolvers: Record<string, Resolver>;
 }
 
-export type RQLOptions<T> = {
+export type RQLOptions<T extends Object> = {
   root: {
     new (args: {}): S.Schema.To<S.Schema<T>> & Resolvers;
     struct: S.Schema<T>;
@@ -19,32 +19,31 @@ export type RQLOptions<T> = {
   schema: S.Schema<any>[];
 };
 
-export class RQL<T> {
+export class RQL<T extends Object> {
   private _rootClass: RQLOptions<T>['root'];
-  private _rootSchema: S.Schema<T & Resolvers>;
   private _root: T & Resolvers;
   private _schema: RQLOptions<T>['schema'];
 
   constructor({ root: Root, schema }: RQLOptions<T>) {
     this._rootClass = Root;
-    this._rootSchema = S.instanceOf(Root);
     this._root = new Root({});
     this._schema = schema;
   }
 
-  get query(): ResolverFunctions<S.Schema.To<typeof this._rootSchema>> {
+  query(query: Query<S.Schema.To<typeof this._rootClass.struct>>): any {
     // TODO(wittjosiah): Wrap with schema validation.
-    return this._root.resolvers as ResolverFunctions<S.Schema.To<typeof this._rootSchema>>;
+    // return this._root.resolvers as ResolverFunctions<S.Schema.To<typeof this._rootSchema>>;
+    return this._root.resolvers;
   }
 }
 
-export const resolver = <TArgs = any, TResult = any>({
-  args = S.any,
+export const resolver = <TArgs = never, TResult = any>({
+  args,
   result,
 }: {
   args?: S.Schema<TArgs>;
   result: S.Schema<TResult>;
-}) => S.optional(S.struct({ __args: args, __result: result }));
+}) => S.optional(S.struct({ __args: args ?? S.never, __result: result }));
 
 class Section extends S.TaggedClass<Section>()('plugin-stack/section', {
   content: S.any, // Echo Object
@@ -116,7 +115,16 @@ export const rql = new RQL({
   schema: [Folder, Stack],
 });
 
-const stacks = await rql.query.listStacks();
+const stacks = await rql.query({
+  listStacks: {
+    id: true,
+    name: true,
+    sections: {
+      content: true,
+      opened: true,
+    },
+  },
+});
 
 console.log(stacks);
 
@@ -124,25 +132,75 @@ console.log(stacks);
 
 // TODO(wittjosiah): Which of these need to be exposed?
 
+type ElementType<T> = T extends (infer U)[] | readonly (infer U)[] ? U : T;
+
+type Object = Record<string | symbol | number, any>;
+
 type MaybePromise<T> = T | Promise<T>;
 
-type CustomResolver<TArgs = any, TResult extends {} = {}> = {
+type CustomResolver<TArgs, TResult> = {
   __args: TArgs;
   __result: TResult;
 };
 
-type ResolverResult<T> =
-  T extends CustomResolver<any, infer TResult>
-    ? TResult
-    : T extends {}
-      ? { [key in keyof T]: ResolverResult<T[key]> }
-      : T;
+type ResolverFunction<TArgs, TResult> = (args?: TArgs) => MaybePromise<TResult>;
 
-type ResolverFunction<TArgs, TResult extends {}> = (args?: TArgs) => MaybePromise<ResolverResult<TResult>>;
-
-type FieldToResolver<T> =
-  T extends CustomResolver<infer TArgs, infer TResult> ? ResolverFunction<TArgs, TResult> : never;
-
-type ResolverFunctions<T extends {}> = Required<{
-  [key in keyof Omit<T, 'resolvers'>]: FieldToResolver<T[key]>;
+type Query<T extends Object> = Partial<{
+  [key in keyof Omit<T, '_tag' | 'resolvers'>]?: QueryField<T[key]>;
 }>;
+
+type QueryField<T> =
+  T extends CustomResolver<never, infer TResult>
+    ? QueryResolverResult<TResult>
+    : T extends CustomResolver<infer TArgs, infer TResult>
+      ? ResolverFunction<TArgs, QueryResolverResult<TResult>>
+      : boolean;
+
+type QueryResolverResult<T> = ElementType<T> extends Object ? Query<ElementType<T>> : ElementType<T>;
+
+type QueryResult<T extends Object = {}, TQuery extends Query<T> = Query<T>> = Required<{
+  [key in keyof Pick<T, keyof TQuery>]: ResultField<T[key]>;
+}>;
+
+type ResultField<T> =
+  T extends CustomResolver<any, infer TResult>
+    ? TResult extends any[] | readonly any[]
+      ? ElementType<TResult> extends Object
+        ? QueryResult<ElementType<TResult>>[]
+        : TResult
+      : TResult extends Object
+        ? QueryResult<TResult>
+        : TResult
+    : T;
+
+export type A = Query<S.Schema.To<typeof Root.struct>>;
+export type B = QueryResult<S.Schema.To<typeof Root.struct>>;
+export type C = ResultField<S.Schema.To<typeof Folder.struct>['items']>;
+export type D = QueryResult<S.Schema.To<typeof Folder.struct>>;
+
+export type E = ResultField<{
+  readonly __args: never;
+  readonly __result: readonly any[];
+}>;
+export type F = QueryResult<any>;
+
+export const a = {
+  listStacks: {
+    id: true,
+    sections: {
+      content: true,
+      opened: true,
+    },
+  },
+} satisfies Query<S.Schema.To<typeof Root.struct>>;
+
+// TODO(wittjosiah): Application of the query structure is not applied recursively.
+export const b: QueryResult<S.Schema.To<typeof Root.struct>, typeof a> = {
+  listStacks: [
+    {
+      id: '1',
+      name: 'Stack 1',
+      sections: [{ content: 'Section 1', opened: true }],
+    },
+  ],
+};
