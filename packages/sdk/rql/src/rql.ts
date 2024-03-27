3f2e41e220bf7058a4ac1f352398604fa710a032
Josiah Witt <josiah@witt.life>
2024-03-27 08:27:53 -0400

    static resolvers

--- packages/sdk/rql/src/rql.ts
+++ packages/sdk/rql/src/rql.ts
@@ -4,81 +4,67 @@
 
 import * as S from '@effect/schema/Schema';
 
-type Resolvers<TArgs extends Record<string, S.Schema<any>>, TResults extends S.Struct.Fields> = {
-  // TODO(wittjosiah): Support args of never for no args.
-  [k in keyof Pick<TResults, keyof TArgs>]: (
-    args?: S.Schema.Type<TArgs[k]>,
-  ) => MaybePromise<S.Schema.Type<TResults[k]>>;
-};
-
-interface ResolverClass<TArgs extends Record<string, S.Schema<any>>, TResults extends S.Struct.Fields> {
-  args: TArgs;
-  resolvers: Resolvers<TArgs, TResults>;
-}
-
 export type RQLOptions<
   T extends Object,
+  TFields extends S.Struct.Fields,
   TArgs extends Record<string, S.Schema<any>>,
-  TResults extends S.Struct.Fields,
 > = {
-  root: {
-    // TODO(wittjosiah): Remove args for root.
-    new (args: any): T & ResolverClass<TArgs, TResults>;
-    fields: S.Struct.Fields;
-  };
-  schema: S.Schema<any>[];
+  root: QuerySchema<T, TFields, TArgs>;
+  schema?: TypeClass<any>[];
 };
 
-export class RQL<T extends Object, TArgs extends Record<string, S.Schema<any>>, TResults extends S.Struct.Fields> {
-  private _rootClass: RQLOptions<T, TArgs, TResults>['root'];
-  private _root: T & ResolverClass<TArgs, TResults>;
+export class RQL<T extends Object, TFields extends S.Struct.Fields, TArgs extends Record<string, S.Schema<any>>> {
+  private _root: RQLOptions<T, TFields, TArgs>['root'];
+  private _schema: Required<RQLOptions<T, TFields, TArgs>>['schema'];
 
-  constructor({ root: Root, schema }: RQLOptions<T, TArgs, TResults>) {
-    this._rootClass = Root;
-    this._root = new Root({});
+  constructor({ root, schema = [] }: RQLOptions<T, TFields, TArgs>) {
+    this._root = root;
+    this._schema = [root, ...schema];
   }
 
-  async query(query: Query<T>): Promise<QueryResult<T, typeof query>> {
+  async query(
+    query: Query<T, TypeClassByTypename<typeof this._schema>>,
+  ): Promise<QueryResult<T, TypeClassByTypename<typeof this._schema>, typeof query>> {
     // TODO(wittjosiah): Make recursive.
     const keys = Object.keys(query);
     const result = keys.reduce(
       (acc, key) => {
         const resolver = this._root.resolvers[key];
-        const resolverSchema = this._rootClass.fields[key];
+        const argsSchema = this._root.args[key];
+        const resultSchema = this._root.fields[key];
         const args = query[key];
-        if (!!resolver && resolverSchema && args) {
+        if (!!resolver && resultSchema && argsSchema && args) {
           // TODO(wittjosiah): Validate args. Call resolver. Validate result.
           return { ...acc, [key]: key as any };
         } else {
           return acc;
         }
       },
-      {} as QueryResult<T, typeof query>,
+      {} as QueryResult<T, TypeClassByTypename<typeof this._schema>, typeof query>,
     );
 
     return result;
   }
 }
 
+// Example
+
 class Section extends S.TaggedClass<Section>()('plugin-stack/section', {
-  content: S.any, // Echo Object
+  content: S.unknown, // Echo Object
   opened: S.boolean, // Ephemeral state
 }) {}
 
-class Stack
-  extends S.TaggedClass<Stack>()('plugin-stack/stack', {
-    id: S.string,
-    name: S.string,
-    sections: S.array(Section),
-  })
-  implements ResolverClass<typeof Stack.prototype.args, typeof Stack.fields>
-{
-  readonly args = {
+class Stack extends S.TaggedClass<Stack>()('plugin-stack/stack', {
+  id: S.string,
+  name: S.string,
+  sections: S.array(Section),
+}) {
+  static args = {
     // TODO(wittjosiah): Never.
     sections: S.struct({}),
   };
 
-  readonly resolvers: Resolvers<typeof this.args, typeof Stack.fields> = {
+  static resolvers: Resolvers<typeof Stack.args, typeof Stack.fields> = {
     sections: async () => {
       // 1) Query echo for sections.
       // 2) Lookup open state in local storage.
@@ -87,27 +73,24 @@ class Stack
   };
 }
 
-class Folder
-  extends S.TaggedClass<Folder>()('plugin-folder/folder', {
-    id: S.string,
-    name: S.string,
-    items: S.array(
-      S.struct({
-        id: S.string,
-        name: S.string,
-      }),
-    ),
-  })
-  implements ResolverClass<typeof Folder.prototype.args, typeof Folder.fields>
-{
-  readonly args = {
+class Folder extends S.TaggedClass<Folder>()('plugin-folder/folder', {
+  id: S.string,
+  name: S.string,
+  items: S.array(
+    S.struct({
+      id: S.string,
+      name: S.string,
+    }),
+  ),
+}) {
+  static args = {
     items: S.struct({
       limit: S.number,
       offset: S.number,
     }),
   };
 
-  readonly resolvers: Resolvers<typeof this.args, typeof Folder.fields> = {
+  static resolvers: Resolvers<typeof Folder.args, typeof Folder.fields> = {
     items: async (args) => {
       // 1) Query echo for items.
       return [];
@@ -115,20 +98,17 @@ class Folder
   };
 }
 
-class Root
-  extends S.Class<Root>('Root')({
-    listFolders: S.array(Folder),
-    listStacks: S.array(Stack),
-  })
-  implements ResolverClass<typeof Root.prototype.args, typeof Root.fields>
-{
-  readonly args = {
+class Root extends S.Class<Root>('Root')({
+  listFolders: S.array(Folder),
+  listStacks: S.array(Stack),
+}) {
+  static args = {
     listFolders: S.struct({}),
     listStacks: S.struct({}),
   };
 
   // TODO(wittjosiah): Wrapper of S.Class which infers the type of the resolvers.
-  readonly resolvers: Resolvers<typeof this.args, typeof Root.fields> = {
+  static resolvers: Resolvers<typeof Root.args, typeof Root.fields> = {
     listFolders: async () => {
       // 1) Query echo for folders.
       // 2) Lookup open directories in local storage.
@@ -141,21 +121,26 @@ class Root
   };
 }
 
+const schema = [Folder, Stack, Section, Root];
 export const rql = new RQL({
   root: Root,
-  schema: [Folder, Stack],
+  schema,
 });
 
-void rql.query({
-  listStacks: {
-    id: true,
-    name: true,
-    sections: {
-      content: true,
-      opened: true,
+export const query = async () => {
+  const result = await rql.query({
+    listStacks: {
+      id: true,
+      name: true,
+      sections: {
+        content: true,
+        opened: true,
+      },
     },
-  },
-});
+  });
+
+  return result;
+};
 
 // Internal Types
 
@@ -167,59 +152,138 @@ type Object = Record<string | symbol | number, any>;
 
 type MaybePromise<T> = T | Promise<T>;
 
-type ResolverFunction<TArgs, TResult> = (args?: TArgs) => MaybePromise<TResult>;
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
 
-type Query<T> =
-  T extends ResolverClass<infer TArgs, infer TResults>
+type Type<T extends string = string> = {
+  _tag: T;
+};
+
+type TypeClass<T extends Type> = {
+  prototype: T;
+  fields: S.Struct.Fields;
+};
+
+type StaticAndInstanceType<T extends TypeClass<any>> = {
+  instance: T['prototype'];
+  static: T;
+};
+
+// For type A | B, this will compute StaticAndInstanceType<A> | StaticAndInstanceType<B>
+// instead of StaticAndInstanceType<A | B>.
+// TODO(wittjosiah): Name.
+type Magic<T> = T extends infer U ? (U extends TypeClass<any> ? StaticAndInstanceType<U> : never) : never;
+
+type Prototypes<T extends TypeClass<any>[]> = Magic<ElementType<T>>;
+
+type Typename<T> = T extends Type<infer U> ? U : never;
+
+type GroupByTypename<T> =
+  T extends StaticAndInstanceType<infer U>
+    ? U['prototype'] extends Type<infer Tag>
+      ? { [K in Tag]: U }
+      : never
+    : never;
+
+type TypeClassByTypename<T extends TypeClass<any>[]> = S.Simplify<UnionToIntersection<GroupByTypename<Prototypes<T>>>>;
+
+type Resolvers<TArgs extends Record<string, S.Schema<any>>, TResults extends S.Struct.Fields> = {
+  // TODO(wittjosiah): Support args of never for no args.
+  [k in keyof Pick<TResults, keyof TArgs>]: (
+    args?: S.Schema.Type<TArgs[k]>,
+  ) => MaybePromise<S.Schema.Type<TResults[k]>>;
+};
+
+type QuerySchema<T, TFields extends S.Struct.Fields, TArgs extends Record<string, S.Schema<any>>> = S.Schema<T> & {
+  args: TArgs;
+  resolvers: Resolvers<TArgs, TFields>;
+  fields: TFields;
+  prototype: T;
+};
+
+type ClassLookup = Record<string, TypeClass<any>>;
+
+type Query<T, TLookup extends ClassLookup> =
+  T extends QuerySchema<infer TSchema, infer TFields, infer TArgs>
     ? S.Simplify<
         Partial<{
-          [k in keyof Pick<TResults, keyof TArgs>]: QueryField<
-            ResolverFunction<S.Schema.Type<TArgs[k]>, S.Schema.Type<TResults[k]>>
+          [k in keyof Pick<TFields, keyof TArgs>]: QueryField<
+            ResolverFunction<S.Schema.Type<TArgs[k]>, S.Schema.Type<TFields[k]>>,
+            TLookup
           >;
         }> &
           Partial<{
-            [key in keyof Omit<T, 'args' | 'resolvers' | '_tag' | keyof TArgs>]: boolean;
+            [k in keyof Omit<TSchema, 'args' | 'resolvers' | '_tag' | keyof TArgs>]: QueryField<TSchema[k], TLookup>;
           }>
       >
-    : T extends Object
-      ? Partial<{ [key in keyof Omit<T, '_tag'>]: boolean }>
-      : never;
+    : T extends TypeClass<any>
+      ? Query<T['prototype'], TLookup>
+      : T extends Object
+        ? Partial<{ [k in keyof Omit<T, '_tag'>]: QueryField<T[k], TLookup> }>
+        : never;
+
+export type F = Query<typeof Root, TypeClassByTypename<typeof schema>>['listStacks'];
+
+type ResolverFunction<TArgs, TResult> = (args?: TArgs) => MaybePromise<TResult>;
 
 type ArgsQuery<TArgs, TResult> = { args: TArgs; query: TResult };
 
-type QueryField<T> =
+type QueryField<T, TLookup extends ClassLookup> =
   // TODO(wittjosiah): Never for no args.
   T extends ResolverFunction<{}, infer TResult>
-    ? QueryResolverResult<TResult>
+    ? QueryResolverResult<TResult, TLookup>
     : T extends ResolverFunction<infer TArgs, infer TResult>
-      ? // QueryFunction<TArgs, QueryResolverResult<TResult>>
-        ArgsQuery<TArgs, QueryResolverResult<TResult>>
-      : boolean;
+      ? ArgsQuery<TArgs, QueryResolverResult<TResult, TLookup>>
+      : T extends any[] | readonly any[]
+        ? QueryField<ElementType<T>, TLookup>
+        : T extends Object
+          ? boolean | Query<T, TLookup>
+          : boolean;
 
-type QueryResolverResult<T> = ElementType<T> extends Object ? Query<ElementType<T>> : ElementType<T>;
+type GetClass<T, TLookup extends ClassLookup> = Typename<T> extends never ? T : TLookup[Typename<T>];
 
-type QueryResult<T extends Object = {}, TQuery extends Query<T> = Query<T>> = Required<{
-  [key in keyof Pick<T, keyof TQuery>]: ResultField<T[key], TQuery[key]>;
-}>;
+type QueryResolverResult<T, TLookup extends ClassLookup> =
+  ElementType<T> extends Object ? Query<GetClass<ElementType<T>, TLookup>, TLookup> : ElementType<T>;
+
+type QueryResult<T, TLookup extends ClassLookup, TQuery extends Object> =
+  T extends QuerySchema<any, infer TFields, any>
+    ? Required<{
+        [k in keyof Pick<TFields, keyof TQuery>]: ResultField<S.Schema.Type<TFields[k]>, TLookup, TQuery[k]>;
+      }>
+    : T extends Object
+      ? Required<{
+          [k in keyof Pick<T, keyof TQuery>]: ResultField<T[k], TLookup, TQuery[k]>;
+        }>
+      : never;
 
 // TODO(wittjosiah): Why `undefined | unknown`?
-type ResultField<T, TQuery extends QueryField<T> | undefined | unknown> = T extends any[] | readonly any[]
-  ? ResultField<ElementType<T>, TQuery>[]
-  : T extends ResolverClass<any, infer TResult>
-    ? ResultFieldResolver<S.Schema.Type<S.struct<TResult>>, TQuery>
-    : ResultFieldResolver<T, TQuery>;
+type ResultField<
+  T,
+  TLookup extends ClassLookup,
+  TQuery extends QueryField<T, TLookup> | undefined | unknown,
+> = T extends any[] | readonly any[]
+  ? ResultField<ElementType<T>, TLookup, TQuery>[]
+  : T extends Type
+    ? ResultField<GetClass<T, TLookup>, TLookup, TQuery>
+    : T extends QuerySchema<any, infer TResult, any>
+      ? ResultFieldResolver<S.Schema.Type<S.struct<TResult>>, TLookup, TQuery>
+      : ResultFieldResolver<T, TLookup, TQuery>;
 
-// TODO(wittjosiah): Fix type errors.
-type ResultFieldResolver<TResult, TQuery extends QueryField<TResult> | undefined | unknown> = TResult extends Object
+type ResultFieldResolver<
+  TResult,
+  TLookup extends ClassLookup,
+  TQuery extends QueryField<TResult, TLookup> | undefined | unknown,
+> = TResult extends Object
   ? TQuery extends ArgsQuery<any, infer TQueryResult>
-    ? QueryResult<TResult, TQueryResult>
+    ? TQueryResult extends Object
+      ? QueryResult<TResult, TLookup, TQueryResult>
+      : never
     : TQuery extends Object
-      ? QueryResult<TResult, TQuery>
+      ? QueryResult<TResult, TLookup, TQuery>
       : never
   : TResult;
 
-export type A = Query<typeof Root.prototype>;
-export type B = QueryResult<typeof Root.prototype>;
+export type A = Query<typeof Root, TypeClassByTypename<typeof schema>>;
+export type B = QueryResult<typeof Root, TypeClassByTypename<typeof schema>, A>;
 
 export const a = {
   listFolders: {
@@ -237,9 +301,9 @@ export const a = {
       content: true,
     },
   },
-} satisfies Query<typeof Root.prototype>;
+} satisfies A;
 
-export const b: QueryResult<typeof Root.prototype, typeof a> = {
+export const b: QueryResult<typeof Root.prototype, TypeClassByTypename<typeof schema>, typeof a> = {
   listFolders: [
     {
       id: '1',
@@ -254,10 +318,10 @@ export const b: QueryResult<typeof Root.prototype, typeof a> = {
   ],
 };
 
-type C = Query<typeof Folder.prototype>;
+type C = Query<typeof Folder, TypeClassByTypename<typeof schema>>;
 export const c = {
   name: true,
   items: { args: { limit: 10, offset: 0 }, query: { name: true } },
 } satisfies C;
 
-type D = QueryResult<typeof Folder.prototype, typeof c>;
+export type D = QueryResult<typeof Folder.prototype, TypeClassByTypename<typeof schema>, typeof c>;
